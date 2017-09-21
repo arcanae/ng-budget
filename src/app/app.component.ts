fe64b6a6e4e4302d0082607e7494536bc7054a2e
Arcanae <justin.courdesse@gmail.com>
2017-09-21 14:45:16 +0200

    css and buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Pay } from '../class/pay.class';
-
+import { FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +13,37 @@ export class AppComponent {
   desc:string = "";
   amount:number;
   type:string = "";
-  
+  display:boolean = false;
+
+
   myArray = [];
+
+  move() {
+    this.display = true;
+  }
+
   submit() {
     this.myArray.push(new Pay (this.date, this.desc, this.amount, this.type));
     console.log(this.myArray);
   }
+
+  rm(index:number){
+    this.myArray.splice(index,1);
+  }
+
+  getCSSClasses(amount:number) {
+    let cssClasses;
+    if(amount < 0) {  
+       cssClasses = {
+         'red': true,
+         'green': false 
+       }	
+    } else {  
+       cssClasses = {
+         'red': false,
+        'green': true 
+       }	
+    }
+    return cssClasses;
+  }	
 }
